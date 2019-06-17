a1d0bf4d0c7966f14aa90b0fd288d9ef2c039fb8
TamAnh <tamptit2016@gmail.com>
2019-06-18 03:15:09 +0700

    Quizapp

---
+++ app/src/main/java/vn/edu/ptit/quizapp/monhoc/ToanHocFragment.java
@@ -0,0 +1,68 @@
+package vn.edu.ptit.quizapp.monhoc;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+import vn.edu.ptit.quizapp.MainActivity;
+import vn.edu.ptit.quizapp.R;
+import vn.edu.ptit.quizapp.slide.ScreenSlideActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ToanHocFragment extends Fragment {
+
+    ExamAdapter examAdapter;
+    GridView gvExam;
+    ArrayList<Exam> arr_exam= new ArrayList<Exam>();
+
+    public ToanHocFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Môn GDCD");
+        return inflater.inflate(R.layout.fragment_toan_hoc, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        gvExam=(GridView) getActivity().findViewById(R.id.gvExam);
+        arr_exam.add(new Exam("Đề số 1"));
+        arr_exam.add(new Exam("Đề số 2"));
+        arr_exam.add(new Exam("Đề số 3"));
+        arr_exam.add(new Exam("Đề số 4"));
+        arr_exam.add(new Exam("Đề số 5"));
+        arr_exam.add(new Exam("Đề số 6"));
+
+        examAdapter=new ExamAdapter(getActivity(),arr_exam);
+        gvExam.setAdapter(examAdapter);
+        gvExam.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent= new Intent(getActivity(), ScreenSlideActivity.class);
+                intent.putExtra("num_exam",i+1);
+                intent.putExtra("subject","gdcd");
+                intent.putExtra("test","yes");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ repo/README.md
@@ -0,0 +1,1 @@
+# mp-n2_21_quizapp
